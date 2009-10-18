12707216efff3cc908599afba2e78bf697555c22
Nicolas Buduroi <nbuduroi@gmail.com>
2009-10-18 17:27:45 -0400

    Made adapters keep track of server instances and added some helper functions, based on Adrian Cuthbertson code.

--- src/compojure/adapters.clj
+++ src/compojure/adapters.clj
@@ -8,19 +8,51 @@
 
 (ns compojure.adapters
   "Defines some adapters which provide a concise way of attaching a
-  handler to a web server."
+  handler to a web server. It also keep server instances and provides an
+  interface to start and stop them."
   (:require [compojure.server.jetty :as jetty]
             [compojure.server.grizzly :as grizzly])
   (:use compojure.http.servlet))
+
+(def *servers* (ref {}))
+
+(defn start-server [port]
+  (.start (@*servers* port)))
+
+(defn stop-server [port]
+  (.stop (@*servers* port)))
+
+(defn restart-server [port]
+  (stop-server port)
+  (start-server port))
+
+(defn start-all-servers []
+  (doseq [server (vals @*servers*)]
+    (.start server)))
+
+(defn stop-all-servers []
+  (doseq [server (vals @*servers*)]
+    (.stop server)))
+
+(defn restart-all-servers []
+  (stop-all-servers)
+  (start-all-servers))
+
+(defn dissoc-server [port]
+  (dosync (alter *servers* dissoc port)))
 
 (defmacro make-adapter [server]
   `(defn ~server
      ([~'handler] (~server {} ~'handler))
      ([~'options ~'handler]
-        (~(symbol (str server "/run-server"))
-          ~'options
-          "/*"
-          (~'servlet ~'handler)))))
+        (let [~'server (~(symbol (str server "/" server "-server"))
+                        ~'options
+                        "/*"
+                        (~'servlet ~'handler))
+              ~'port (~'options :port 80)]
+          (.start ~'server)
+          (dosync (alter ~'*servers* assoc ~'port ~'server))
+          ~'server))))
 
 (make-adapter jetty)
 (make-adapter grizzly)
